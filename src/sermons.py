4afa4f6a1784c4bca54f10d779db5d7756229659
Nathan Blaubach <nathanblaubach@gmail.com>
2026-02-15 20:45:33 -0600

    Validate form before processing

--- src/sermons.py
+++ src/sermons.py
@@ -1,3 +1,5 @@
+from os.path import isfile
+
 from recording_metadata_form import RecordingMetadataForm
 from recording_media_writer import RecordingMediaWriter
 from recording_metadata_writer import RecordingMetadataWriter
@@ -8,6 +10,14 @@ def main():
     media_writer = RecordingMediaWriter()
 
     metadata = metadata_form.get_recording_metadata()
+    fields_are_empty = not all([
+        metadata.audio_file_path,
+        metadata.title,
+        metadata.date,
+        metadata.speaker_name,
+    ])
+    if fields_are_empty or not isfile(metadata.audio_file_path):
+        return
     metadata_writer.write(metadata)
     media_writer.write(metadata.audio_file_path)
 
